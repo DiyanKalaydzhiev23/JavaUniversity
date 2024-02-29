ba5fedc3f40a3ea68b0073a26d43aa8783ad4cc1
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2024-02-29 16:09:19 +0200

    Added more solutions

--- ProgrammingInJavaUniversity/FirstLecture/src/Main.java
+++ ProgrammingInJavaUniversity/FirstLecture/src/Main.java
@@ -1,10 +1,20 @@
-public class Main {
-    public static boolean isLeapYear(int year) {
-        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-    }
+import VideoPlatform.Video;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Main {
     public static void main(String[] args) {
-        int year = 2024;
-        System.out.println(isLeapYear(year));
+        Video video1 = new Video("Hi my name is", BigDecimal.valueOf(2.1), 0, 1000, LocalDate.now());
+        Video video2 = new Video("Hi my name is", BigDecimal.valueOf(2.3), 0, 2334, LocalDate.now());
+
+        BigDecimal sum = video2.getDuration().add(video1.getDuration());
+        System.out.println(sum);
+
+        if (video2.isLonger(video1)) {
+            System.out.println("Longer");
+        }
+
+        video2.increaseDuration(BigDecimal.valueOf(12));
     }
 }
---
+++ ProgrammingInJavaUniversity/FirstLecture/src/VideoPlatform/Video.java
@@ -0,0 +1,70 @@
+package VideoPlatform;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Video {
+    // default encapsulation is visible in the package
+    private static final BigDecimal maxDuration = BigDecimal.valueOf(10.00);
+    private final String name;
+    private BigDecimal duration;
+    private final long id;
+    private final int likes;
+    private final LocalDate dateUploaded;
+
+    public Video(long id) {
+        this.id = id;
+        this.name = "";
+        this.duration = BigDecimal.valueOf(0.0);
+        this.likes = 0;
+        this.dateUploaded = LocalDate.now();
+    }
+
+    public Video(String name, BigDecimal duration, int likes, long id, LocalDate time) {
+        this.name = name;
+        this.duration = duration;
+        this.likes = likes;
+        this.id = id;
+        this.dateUploaded = time;
+    }
+
+    public void increaseDuration(BigDecimal bonusDuration) {
+        BigDecimal newDuration = this.getDuration().add(bonusDuration);
+
+        if (newDuration.compareTo(maxDuration) < 0) {
+            this.setDuration(newDuration);
+            System.out.println("Changed duration");
+        } else {
+            System.out.println("Cannot change duration");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getDuration() {
+        return duration;
+    }
+
+    public void setDuration(BigDecimal duration) {
+        this.duration = duration;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public boolean isLonger(Video video2) {
+        return this.getDuration().compareTo(video2.getDuration()) > 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Video [name=" + name + ", duration=" + duration + ", likes]";
+    }
+}
